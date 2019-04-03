cb0746502732677d8d36f8c7b4db8dc7a5a6ceb6
Joshua Bassin <jbassin@bu.edu>
2019-04-03 12:09:51 -0400

    FormatLine now gets line numbers

--- client/src/controllers/compendium/CompendiumRender.jsx
+++ client/src/controllers/compendium/CompendiumRender.jsx
@@ -4,15 +4,15 @@ import * as R from 'ramda'
 export default class CompendiumRender extends Component {
     formatText = (text) => {
         const splitText = R.split('\n', text);
-        return R.map(this.formatLine, splitText);
+        return R.addIndex(R.map)(this.formatLine, splitText);
     };
 
-    formatLine = (line) => {
-        const headerTest = line.match(/(?<numbers>^#+\s)(?<header>.+)/g);
-        if (headerTest) {
+    formatLine = (line, lineNum) => {
+        const headerTest = R.match(/(?<numbers>^#+\s)(?<header>.+)/g, line);
+        if (!R.isEmpty(headerTest)) {
             const [intensity, header] = R.split(/ (.*)/, R.head(headerTest));
             const headerMargin = {
-                marginTop: '10px',
+                marginTop: lineNum === 0 ? '0' : '10px',
             };
 
             return (
@@ -30,6 +30,10 @@ export default class CompendiumRender extends Component {
                 children={line}/>
         );
     };
+
+    // transformText = (line) => {
+    //   const boldTest = line.match
+    // };
 
     render = () => {
         return (
