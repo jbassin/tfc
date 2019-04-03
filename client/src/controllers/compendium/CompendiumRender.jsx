a6382af47e7fb44355bd2d7998a2b6e29149d095
Joshua Bassin <jbassin@bu.edu>
2019-04-03 13:29:40 -0400

    Added bold, italics, and monospace

--- client/src/controllers/compendium/CompendiumRender.jsx
+++ client/src/controllers/compendium/CompendiumRender.jsx
@@ -23,17 +23,48 @@ export default class CompendiumRender extends Component {
                     children={header}/>
             );
         }
-
         return (
             <p
-                key={line}
-                children={line}/>
+                key={R.isEmpty(line) ? `${lineNum}` : line}
+                children={this.transformText(line)}/>
         );
     };
 
-    // transformText = (line) => {
-    //   const boldTest = line.match
-    // };
+    transformText = (line) => {
+        let returnArray = [line];
+        returnArray = R.flatten(R.map(this.transformMatch('\\*', 'has-text-weight-bold'), returnArray));
+        returnArray = R.flatten(R.map(this.transformMatch('_', 'is-italic'), returnArray));
+        returnArray = R.flatten(R.map(this.transformMatch('`', 'is-family-monospace'), returnArray));
+        return R.flatten(returnArray);
+    };
+
+    transformMatch = R.curry((delimiter, className, line) => {
+        if (R.type(line) !== 'String') return line;
+        const tester = R.match(new RegExp(`(${delimiter}.+?${delimiter})`, 'g'), line);
+        if (!R.isEmpty(tester)) {
+            const splitTester = R.split(new RegExp(`(${delimiter}.+?${delimiter})`, 'g'), line);
+            return R.map((item) => {
+                if (this.borderedBy(R.replace('\\', '', delimiter), item)) {
+                    return (
+                        <span
+                            key={item}
+                            className={className}
+                            children={this.transformText(this.dropEnds(item))}/>
+                    );
+                }
+                return this.transformText(item);
+            }, splitTester);
+        }
+        return [line];
+    });
+
+    borderedBy = (delimiter, item) => {
+        return R.startsWith(delimiter, item) && R.endsWith(delimiter, item);
+    };
+
+    dropEnds = (item) => {
+        return R.dropLast(1, R.drop(1, item));
+    };
 
     render = () => {
         return (
